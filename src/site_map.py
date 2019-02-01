dd330a1f39ab671078d78b988744ff9e9c9b80e7
myslak71 <Zabojca2golebi>
2019-02-01 13:17:37 +0100

    update site_map.py

--- src/site_map.py
+++ src/site_map.py
@@ -2,19 +2,40 @@ from requests_html import HTMLSession, HTML
 import requests
 
 
-
-
-
-
-def site_map(url, result=[]):
+def get_title_links(url):
     session = HTMLSession()
+
     try:
         response = session.get(url)
     except requests.exceptions.ConnectionError:
-        print('blad')
+        print('ConnectionError')
         return None
+
     response.html.render()
-    print(response.html.links)
+    title = response.html.find('title', first=True).text
+    links = response.html.absolute_links
+    print(links)
+    links = {link for link in links if link.startswith(url)}
+    return {'title': title, 'links': links}
 
 
-site_map('http://0.0.0.0:8000')
+def site_map(url):
+    result = {url: get_title_links(url)}
+    result_tmp = list(result[url]['links'])
+    # for key, value in result.copy().items():
+    #     urls = value['links']
+    #     for url in urls:
+    #         result[url] = get_title_links(url)
+    #         print(url)
+    for url in result_tmp.copy():
+        if url in result:
+            continue
+        result[url] = get_title_links(url)
+        result_tmp.remove(url)
+        result_tmp.extend(result[url]['links'])
+        print(result[url]['links'])
+    print(result_tmp)
+    return result
+
+
+print(site_map('http://0.0.0.0:8000'))
