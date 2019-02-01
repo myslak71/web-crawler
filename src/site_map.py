bb66f2c534327b355255d945b39d19493ce260f4
myslak71 <Zabojca2golebi>
2019-02-01 13:34:30 +0100

    update site_map.py

--- src/site_map.py
+++ src/site_map.py
@@ -2,7 +2,7 @@ from requests_html import HTMLSession, HTML
 import requests
 
 
-def get_title_links(url):
+def get_title_links(url, domain_url=0):
     session = HTMLSession()
 
     try:
@@ -11,31 +11,30 @@ def get_title_links(url):
         print('ConnectionError')
         return None
 
+    if domain_url == 0:
+        domain_url = url
     response.html.render()
     title = response.html.find('title', first=True).text
     links = response.html.absolute_links
-    print(links)
-    links = {link for link in links if link.startswith(url)}
+    links = {link for link in links if link.startswith(domain_url)}
     return {'title': title, 'links': links}
 
 
-def site_map(url):
-    result = {url: get_title_links(url)}
-    result_tmp = list(result[url]['links'])
-    # for key, value in result.copy().items():
-    #     urls = value['links']
-    #     for url in urls:
-    #         result[url] = get_title_links(url)
-    #         print(url)
-    for url in result_tmp.copy():
-        if url in result:
-            continue
-        result[url] = get_title_links(url)
-        result_tmp.remove(url)
-        result_tmp.extend(result[url]['links'])
-        print(result[url]['links'])
-    print(result_tmp)
+def site_map(domain_url):
+    result = {domain_url: get_title_links(domain_url)}
+    result_tmp = list(result[domain_url]['links'])
+    while True:
+        for url in result_tmp.copy():
+            if url in result:
+                result_tmp.remove(url)
+                continue
+            result[url] = get_title_links(url, domain_url)
+            result_tmp.extend(result[url]['links'])
+        print(result_tmp)
+        if result_tmp == []:
+            break
     return result
 
 
 print(site_map('http://0.0.0.0:8000'))
+
