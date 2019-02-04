f0b5d98c7e78d892f371ba2bc1038578537f1ac5
myslak71 <Zabojca2golebi>
2019-02-04 20:37:48 +0100

    update site_map and README.md

--- web_crawler/cli.py
+++ web_crawler/cli.py
@@ -2,21 +2,19 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 from web_crawler.site_map import site_map
 
-
 description = """Description"""
+
 
 def get_parser():
     parser = ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
     required = parser.add_argument_group('required arguments')
-    required.add_argument('-u', '--url', help='Input CSV file path', required=True)
+    required.add_argument('-u', '--url', help='Url to star crawl from', required=True)
     return parser
 
 
 def main():
     parser = get_parser().parse_args()
-
-    # processer = ReportProcesser()
-    # processer.process_csv_report(parser.input, parser.output, parser.errors)
+    site_map(parser.url)
 
 
 if __name__ == '__main__':
