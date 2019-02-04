b1db36132b46898214320c8ef3ad472043b29398
myslak71 <Zabojca2golebi>
2019-02-04 16:46:06 +0100

    update tests and site_map

---
+++ tests/fixtures/responses.py
@@ -0,0 +1,28 @@
+body_no_links = """
+<head>
+    <title>No links</title>
+</head>
+</html>
+"""
+
+body_index_empty_link = """
+<head>
+    <title>Index empty link</title>
+</head>
+<body>
+<a href="http://0.0.0.0/site.html">Site</a>
+</body>
+</html>
+"""
+
+body_index_site_link = """
+<head>
+    <title>Test</title>
+</head>
+<body>
+<a href="http://0.0.0.0/site.html">Site</a>
+</body>
+</html>
+"""
+
+body_site_empty_link
--- tests/test_site_map.py
+++ tests/test_site_map.py
@@ -1,40 +1,44 @@
 import pytest
 
 from mock import Mock, patch
-from web_crawler.site_map import *
 import responses
 
-body = """
-<head>
-	<title>Test</title>
-</head>
-</html>
-"""
+from web_crawler.site_map import *
+from tests.fixtures.responses import *
 
 
 @patch('requests_html.HTML.render')
 @responses.activate
 def test_get_site_data_no_links(mock_render):
-    responses.add(responses.GET, 'http://0.0.0.0', body=body, status=200, content_type='text/html')
+    responses.add(responses.GET, 'http://0.0.0.0', body=body_no_links, status=200, content_type='text/html')
     assert site_map('http://0.0.0.0') == {'http://0.0.0.0': {'title': 'Test', 'links': set()}}
 
 
 @patch('requests_html.HTML.render')
 @responses.activate
 def test_get_site_data_page_not_found(mock_render):
-    responses.add(responses.GET, 'http://0.0.0.0', body=body, status=404, content_type='text/html')
+    responses.add(responses.GET, 'http://0.0.0.0', status=404, content_type='text/html')
     assert site_map('http://0.0.0.0') is None
 
 
 @patch('requests_html.HTML.render')
 @responses.activate
 def test_get_site_data_invalid_content_type(mock_render):
-    responses.add(responses.GET, 'http://0.0.0.0', body=body, status=200, content_type='text/plain')
+    responses.add(responses.GET, 'http://0.0.0.0', status=200, content_type='text/plain')
     assert site_map('http://0.0.0.0') is None
+
+
+@patch('requests_html.HTML.render')
+@responses.activate
+def test_site_map_one_empty_link(mock_render):
+    responses.add(responses.GET, 'http://0.0.0.0', body=body_index_empty_link, status=200, content_type='text/html')
+    responses.add(responses.GET, 'http://0.0.0.0/site', status=404)
+    assert site_map('http://0.0.0.0') == {'http://0.0.0.0': {'links': {'http://0.0.0.0/site.html'}, 'title': 'Test'}}
 
 
 # @patch('requests_html.HTML.render')
 # @responses.activate
-# def test_site_map(mock_render):
-#     responses.add(responses.GET, 'http://0.0.0.0', body=body, status=200, content_type='text/plain')
-#     assert site_map('http://0.0.0.0') is None
+# def test_site_map_one_empty_link(mock_render):
+#     responses.add(responses.GET, 'http://0.0.0.0', body=body_index_empty_link, status=200, content_type='text/html')
+#     responses.add(responses.GET, 'http://0.0.0.0/site', status=404)
+#     assert site_map('http://0.0.0.0') == {'http://0.0.0.0': {'links': {'http://0.0.0.0/site.html'}, 'title': 'Test'}}
