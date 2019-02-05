7593bb052b669aa1e960469c2fdbdb523cb7f508
myslak71 <Zabojca2golebi>
2019-02-05 09:53:40 +0100

    refactor

--- web_crawler/errors.py
+++ web_crawler/errors.py
@@ -2,6 +2,4 @@ class InvalidContentType(Exception):
     def __init__(self, content_type):
         self.content_type = content_type
 
-    def __str__(self):
-        return f'Invalid Content-Type. Expected text/html, given {self.content_type}'
 
