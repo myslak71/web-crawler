5eaca15e19b56b6a2460e6bae9ebfa55429a6a56
myslak71 <Zabojca2golebi>
2019-02-17 12:58:43 +0100

    reformat

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import os
 
 from setuptools import setup, find_packages
 
-DIR_PATH = os.path.dirname(
-    os.path.abspath(__file__))
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(DIR_PATH, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
-requires = ['requests-html==0.9.0','pyyaml==3.13']
+requires = ['requests-html==0.9.0', 'pyyaml==3.13']
 
 setup(
     name='web_crawler',
@@ -16,7 +15,7 @@ setup(
     packages=find_packages(exclude=('tests', 'example')),
     author='Kornel Szurek',
     author_email='kornel.szurek@protonmail.com',
-    description='Web Crawler for Clearcode',
+    description='Web Crawler',
     long_description=long_description,
     install_requires=requires,
     python_requires=">=3.7",
